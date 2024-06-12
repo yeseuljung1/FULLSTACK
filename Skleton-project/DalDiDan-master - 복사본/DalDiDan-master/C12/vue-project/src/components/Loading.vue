643eacad83083fb87978d6bb5940eaa96715ac4d
jungyeseul/git <jys033022@naver.com>
2024-06-12 09:20:56 +0900

    240612

--- Skleton-project/DalDiDan-master - 복사본/DalDiDan-master/C12/vue-project/src/components/Loading.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <VueCsspin message="Loading" spin-style="cp-flip" />
-</template>
-
-<script setup>
-import { VueCsspin } from 'vue-csspin';
-import 'vue-csspin/dist/vue-csspin.css';
-</script>
--- Skleton-project/DalDiDan-master - 복사본/DalDiDan-master/C12/vue-project/src/pages/About.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div class="card card-body">
-        <h2>Card Recommendation</h2>
-    </div>
-</template>
