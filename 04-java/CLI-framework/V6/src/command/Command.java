d25de84c634ca31ac6d7b3c35a1dbedb63b7bd1f
jungyeseul/git <jys033022@naver.com>
2024-07-23 16:31:17 +0900

    24-07-23

--- 04-java/CLI-framework/V6/src/command/Command.java
+++ 04-java/CLI-framework/V6/src/command/Command.java
@@ -1,4 +1,4 @@
-package org.scoula.lib.cli.command;
+package command;
 
 public interface Command {
 //    추상 메소드이므로 재정의 필요
--- 04-java/TodoApp/src/main/java/org/example/command/Command.java
+++
@@ -1,6 +0,0 @@
-package org.example.command;
-
-public interface Command {
-//    추상 메소드이므로 재정의 필요
-    void execute();
-}
