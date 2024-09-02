3d0bc22fa7b61e34dd8b73288ea772f035b1cb18
jungyeseul/git <jys033022@naver.com>
2024-09-02 17:01:41 +0900

    240902

--- 09_Vue+Spring/frontend/src/router/board.js
+++ 09_Vue+Spring/frontend/src/router/board.js
@@ -1,7 +1,26 @@
 export default [
     {
+        // 게시판 리스트 페이지에 대한 라우트
         path: '/board/list',
         name: 'board/list',
         component: () => import('../pages/board/BoardListPage.vue'),
+    },
+    {
+        //게시판 상세페이지에 대한 라우트
+        path: '/board/detail/:no',
+        name: 'board/detail',
+        component: () => import('../pages/board/BoardDetailPage.vue'),
+    },
+    {
+        // 게시판 작성 페이지에 대한 라우트
+        path: '/board/create',
+        name: 'board/create',
+        component: () => import('../pages/board/BoardCreatePage.vue'),
+    },
+    {
+        // 게시판 수정 페이지에 대한 라우트
+        path: '/board/update/:no',
+        name: 'board/update',
+        component: () => import('../pages/board/BoardUpdatePage.vue'),
     },
 ];
